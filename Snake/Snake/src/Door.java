a695d1a531da7eaa5320d78ca857cd4ab8b87416
DiggiSick <79170319+DiggiSick@users.noreply.github.com>
2021-02-23 18:15:03 +0100

    Delete Snake/Snake directory

--- Snake/Snake/src/Door.java
+++
@@ -1,7 +0,0 @@
-
-public class Door extends GameCharacter {
-
-	public Door(int x, int y) {
-		super(x, y, '$');
-	}
-}
--- Snake/Snake/src/Trapdoor.java
+++
@@ -1,7 +0,0 @@
-
-public class Trapdoor extends GameCharacter {
-
-	public Trapdoor(int x, int y) {
-		super(x, y, '.');
-	}
-}
